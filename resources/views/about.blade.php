c53d436788e44c9517f09c989dbbc414777fd160
JJET88 <aungnyithegreat@gmail.com>
2023-09-08 18:14:47 +0630

    route & view

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -5,7 +5,9 @@
 @endsection
 
 @section('home')
+    <div class=" p-3">
 
-    <h1>I'm about page.</h1> 
-    <h1>My name is {{$name}}</h1>   
+        <h1>I'm about page.</h1> 
+        <h1>My name is {{$name}}</h1>   
+    </div>
 @endsection
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,6 +5,10 @@
 @endsection
 
 @section('home')
+    <div class=" p-3">
 
-    <h1>I'm home page.</h1>    
+        <h1>I'm home page.</h1> 
+        <h1>{{"<script> alert('hello')</script>"}}</h1>  
+        <h2>{{$data}}</h2> 
+    </div>
 @endsection
